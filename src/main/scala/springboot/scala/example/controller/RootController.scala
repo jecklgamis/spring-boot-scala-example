30d8ba9b899ab7f36191e8facb2b337e87586da8
Jerrico Gamis <jecklgamis@gmail.com>
2019-06-05 20:00:22 +1000

    Update root controller

--- src/main/scala/springboot/scala/example/controller/RootController.scala
+++ src/main/scala/springboot/scala/example/controller/RootController.scala
@@ -3,16 +3,21 @@ package springboot.scala.example.controller
 import java.time.LocalDateTime
 
 import io.micrometer.core.annotation.Timed
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.web.bind.annotation.RequestMethod.GET
 import org.springframework.web.bind.annotation.{RequestMapping, RestController}
 
 @RestController
 class RootController {
+  @Value("${application.name}")
+  val appName: String = null
 
   @RequestMapping(path = Array("/"), method = Array(GET))
   @Timed
   def root(): Map[String, Any] = {
-    Map("message" -> "It works!", "today" -> LocalDateTime.now())
+    Map("name" -> appName,
+      "java.version" -> System.getProperty("java.version"),
+      "now" -> LocalDateTime.now())
   }
 
 }
